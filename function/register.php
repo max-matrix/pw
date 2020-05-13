221cb6f32e3191834949e6627cddee1acc090208
Ignacio <ignacioesses@gmail.com>
2020-05-13 16:41:39 -0300

    Registro de usuarios en DB (falta verificar que el ususuario no exista). Edición en action de contact para que muestre mensaje según función. Renombramos guardar.php como saveComent.php dentro de carpeta function.

--- function/register.php
+++ function/register.php
@@ -1,10 +1,13 @@
 <?php
 
-require_once("../funciones/config.php");
-require_once("../funciones/funciones.php");
+require_once '../mysql-login.php';
+$con = new PDO('mysql:host=' . $hostname . ';port=' . $port . ';dbname=' . $database, $username, $password, $charset);
+
+require_once("../function/config.php");
+require_once("../function/function.php");
 
 // Contenido obligatorio:
-if(empty($_POST["email"]) || empty($_POST["password"])):
+if(empty($_POST["email"]) || empty($_POST["clave"])):
     $_SESSION["estado"] = "error";
     $_SESSION["mensaje"] = "Los campos Email y Contraseña son obligatorios.";
 
@@ -13,19 +16,23 @@ if(empty($_POST["email"]) || empty($_POST["password"])):
 endif;
 
 // Levanto datos de POST:
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$usuario = $_POST["usuario"];
 $email = $_POST["email"];
-$password = $_POST["password"];
-$nuevoUsuario = explode("@",$email)[0];
+$clave = encrypt($_POST["clave"],$_POST["salt"]);
+$salt = $_POST["salt"];
+
+//$nuevoUsuario = explode("@",$email)[0];
 
 // Verifico que el usuario no exista:
-$usuario = !empty($_POST["usuario"]) ? $_POST["usuario"] : $nuevoUsuario;
-
 
 // Creo el nuevo usuario:
-
-
-
-
+$sql = "INSERT INTO usuarios(nombre,apellido,usuario,clave,email,tipo,activo,salt) 
+               VALUES ('$nombre','$apellido','$usuario','$clave','$email','4','1','$salt');";
+               /* echo $sql; */
+       $count = $con->exec($sql);
+	  
 // Mensaje de confirmación:
 $_SESSION["estado"] = "ok";
 $_SESSION["mensaje"] = "Ya podés ingresar con los datos con los que te registraste";
