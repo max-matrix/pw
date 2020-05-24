6a64000e31340103b792568228204c32e1224d45
Ignacio <ignacioesses@gmail.com>
2020-05-24 20:56:37 -0300

    Integración con categorías. Renombramos archivos en admin/class.

---
+++ admin/class/class_categories.php
@@ -0,0 +1,118 @@
+<?php 
+Class Categoria{
+
+    /*conexion a la base*/
+	private $con;
+	
+	public function __construct($con){
+		$this->con = $con;
+	}
+
+	public function getList(){
+		$query = "SELECT id_categoria, nombre, id_padre
+		           FROM categoria";
+        return $this->con->query($query); 
+	}
+	
+	public function get($id_categoria){
+	    $query = "SELECT id_categoria,nombre,id_padre
+		           FROM categoria WHERE id_categoria = ".$id_categoria;
+        $query = $this->con->query($query); 
+			
+		$categoria = $query->fetch(PDO::FETCH_OBJ);
+			
+			// $sql = 'SELECT id_categoria, nombre
+			// 		  FROM categoria  
+			// 		  WHERE id_categoria = '.$categoria->id_categoria;
+					  
+			// foreach($this->con->query($sql) as $permiso){
+			// 	$perfil->permisos[] = $permiso['permiso_id'];
+			// }
+			// echo '<pre>';
+			// var_dump($perfil);echo '</pre>'; 
+
+        return $categoria;
+	}
+
+	public function del($id_categoria){
+		$query = 'SELECT count(1) as cantidad FROM usuarios_perfiles WHERE perfil_id = '.$id_categoria;
+		$consulta = $this->con->query($query)->fetch(PDO::FETCH_OBJ);
+		if($consulta->cantidad == 0){
+			$query = "DELETE FROM categoria WHERE id_categoria = ".$id_categoria."; 
+					  DELETE FROM categoria WHERE id_categoria = ".$id_categoria.";";
+
+			return $this->con->exec($query); 
+		}
+		return 'Categoria asignada a un producto';
+	}
+	
+	/**
+	* Guardo los datos en la base de datos
+	*/    
+	
+	public function save($data){
+		
+            foreach($data as $key => $value){
+				
+				if(!is_array($value)){
+					if($value != null){
+						$columns[]=$key;
+						$datos[]=$value;
+					}
+				}
+			}
+            // echo "<pre>"; var_dump($data);echo "</pre>";
+			// echo "<pre>";var_dump($columns);echo "</pre>";
+			// echo "<pre>";var_dump($datos);echo "</pre>";
+			
+            $sql = "INSERT INTO categoria(".implode(',',$columns).") VALUES('".implode("','",$datos)."')";
+			// echo "<pre>";var_dump($sql);echo "</pre>";
+			
+
+            $this->con->exec($sql);
+			$id_categoria = $this->con->lastInsertId();
+			//echo "<pre>";var_dump($id_categoria);echo "</pre>";
+			
+			// $sql = '';
+			// foreach($data['categorias'] as $categorias){
+			// 	$sql .= 'INSERT INTO categoria(id_categoria,nombre) 
+			// 				VALUES ('.$id_categoria.','.$nombre.');';
+			// }
+			//echo $sql;die();
+
+ 			// $this->con->exec($sql);
+	} 
+	
+	
+
+	
+	public function edit($data){
+			$id_categoria = $data['id_categoria'];
+			//echo "<pre>";  var_dump($data); "</pre>"; die();
+			unset($data['id_categoria']);
+            
+            foreach($data as $key => $value){
+				if(!is_array($value)){
+					if($value != null){	
+						$columns[]=$key." = '".$value."'"; 
+					}
+				}
+			}
+            //echo "<pre>";  var_dump($data); "</pre>"; 
+            $sql = "UPDATE categoria SET ".implode(',',$columns)." WHERE id_categoria = ".$id_categoria;
+            //echo $sql; die();
+            $this->con->exec($sql);
+		 
+			// $sql = 'DELETE FROM categoria WHERE id_categoria= '.$id_categoria;
+			// $this->con->exec($sql);
+			
+			// $sql = '';
+			// foreach($data['categorias'] as $categorias){
+			// 	$sql .= 'INSERT INTO categoria(id_categoria,nombre) 
+			// 				VALUES ('.$id_categoria.','.$nombre.');';
+			// }
+			// $this->con->exec($sql);
+			 
+	} 
+}
+?>
---
+++ admin/section/categories.php
@@ -0,0 +1,68 @@
+<div class="container my-5 text-center">
+    <div class="row justify-content-center">
+        <div class="col-12"> 
+            <h1 class="text-center">Categorías</h1>
+        </div>      
+    </div>
+
+	<div class="row justify-content-center">
+
+		<?php 	
+			$categoriasMenu = 'Categorias';
+			$categorias = new Categoria($con);
+
+			if (isset($_POST['formulario_categorias'])) {
+				if ($_POST['id_categoria'] > 0) {
+					$categorias->edit($_POST);
+				} else {
+					$categorias->save($_POST);
+				}
+				header('Location: index.php?section=categories');
+			}
+			
+			if (isset($_GET['del'])) {
+				$resp = $categorias->del($_GET['del']) 	;
+				if ($resp == 1) {
+					header('Location: index.php?section=categories');
+				}
+				echo '<script>alert("'.$resp.'");</script>';
+			}
+		?>	 
+
+		<div class="col-12">
+			<h2 class="sub-header text-left"> 
+				<a href="index.php?section=categories_abm"><button type="button" class="btn btn-success btn-md" title="Agregar">Agregar Categoría</button></a></h2>
+			<table class="table table-striped">
+				<thead>
+					<tr class="font-weight-bold h5 text-center">
+						<th>#</th>
+						<th>Nombre</th>
+						<th>Id_padre</th>
+						<th>Acciones</th>
+					</tr>
+				</thead>
+				<tbody> 
+					<?php
+						foreach ($categorias->getList() as $categoria) {?>
+
+						<tr>
+							<td><?php echo $categoria['id_categoria'];?>
+							</td>
+							<td><?php echo $categoria['nombre'];?>
+							</td>
+							<td><?php echo $categoria['id_padre'];?>
+							</td>
+							<td>
+								<a	href="index.php?section=categories_abm&edit=<?php echo $categoria['id_categoria']?>"><button
+									type="button" class="btn btn-info btn-md" title="Modificar">Modificar</button></a>
+								
+								<a	href="index.php?section=categories&del=<?php echo $categoria['id_categoria']?>"><button
+									type="button" class="btn btn-danger btn-md" title="Borrar">Eliminar</button></a>
+							</td>
+						</tr>
+					<?php }?>     
+				</tbody>
+			</table>
+		</div>	
+	</div>
+</div>
