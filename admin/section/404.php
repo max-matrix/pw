564636799817e20f0b1738ba1b6620c32edbbbc2
Max <mladeveloper@gmail.com>
2020-07-11 11:02:22 -0300

    Se agrego páginas de error 404 tanto al front como al back

--- admin/section/404.php
+++ admin/section/404.php
@@ -1,1 +1,11 @@
-ERROR
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div class="wrapper-404 d-flex flex-column justify-content-center">
+        <h1>ERROR 404</h1>
+        <h3>No existe la página</h3>
+        <a href="index.php?section=products">Volver al inicio</a>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ section/404.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div class="wrapper-404 d-flex flex-column justify-content-center">
+        <h1>ERROR 404</h1>
+        <h3>No existe la página</h3>
+        <a href="index.php">Volver al inicio</a>
+      </div>
+    </div>
+  </div>
+</div>
